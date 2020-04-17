d7b43f5baf48f66659c625e53fd5ba39b5802a88
JN <git@istalacar.com>
2020-04-17 21:14:14 +0200

    Add more words

--- websocket/actions/getWords.js
+++ websocket/actions/getWords.js
@@ -10,12 +10,12 @@ const WORDS = [
     'Saturn', 'ława', 'dno', 'teatr', 'diament',
     'pociąg', 'spadek', 'żabka', 'placek', 'rzęsa',
     'rzut', 'guma', 'soczewka','kontrakt', 'kiwi',
-    'klucz','masa','jagoda','rura','usta',
-    'ogon', 'dusza', 'królik','superbohater','bal',
+    'klucz', 'masa', 'jagoda', 'rura', 'usta',
+    'ogon', 'dusza', 'królik','superbohater', 'bal',
     'księżniczka','trucizna', 'chochlik', 'klawisz', 'mistrz',
     'stołek', 'kciuk', 'szkoła', 'most', 'bomba',
     'ruletka', 'statek', 'trawa', 'siano', 'lew',
-    'cebula', 'talia', 'paluszki', 'ogień', 'szkło',
+    'talia', 'paluszki', 'ogień', 'szkło',
     'wojna', 'mur', 'pociecha', 'plaża', 'szczyt',
     'zmywacz', 'dzięcioł', 'plik', 'awaria', 'ucho',
     'pazur', 'warsztat', 'węgiel', 'miesiąc', 'warunek',
@@ -23,7 +23,21 @@ const WORDS = [
     'osa', 'torba', 'festiwal', 'słowo', 'funkcja',
     'ponton', 'kogut', 'łabędź', 'krasnal', 'tęcza',
     'zupa', 'film', 'igła', 'rak', 'życie',
-    'pustka', 'belka', 'sieć', 'lód', 'rycerz'
+    'pustka', 'belka', 'sieć', 'lód', 'rycerz',
+    'Żuk', 'geniusz', 'mucha', 'butelka', 'strona',
+    'helikopter', 'aztek', 'pingwin', 'król', 'grabarz', 'niedźwiedź',
+    'papier', 'jaja', 'merkury', 'spadochron', 'praca', 'gotyk', 'punkt',
+    'hak', 'dinozaur', 'samolot', 'pekin', 'strumień', 'księżyc',
+    'ryba', 'funt', 'noc', 'olimp', 'pojazd', 'powietrze', 'bank', 'jowisz', 'port', 'las', 'ekran',
+    'nektar', 'znak', 'ciało', 'marchew', 'china', 'dziobak', 'obcy', 'pudło', 'milioner', 'berlin', 'szpilka', 'ząb',
+    'obsada', 'anioł', 'szpieg', 'truteń', 'stadion', 'pies', 'budowa', 'francuz', 'gniazdko', 'kod', 'woda', 'ambasada',
+    'prawo', 'grzyb', 'wiatr', 'zwoje', 'żuraw', 'przewodnik', 'dzień', 'czekolada', 'kasyno', 'szpital',
+    'teleskop', 'limuzyna', 'ogier', 'gnat', 'róg', 'podkład', 'gołąb', 'dzwon', 'rewolucja',
+    'szczypior', 'splot', 'ameryka', 'pierścień', 'tusz', 'szekspir', 'europa', 'rzym', 'zieleń', 'krówka', 'wieżowiec',
+    'podkowa', 'pasta', 'moskwa', 'fartuch', 'sokół', 'maj', 'gwiazda', 'siekacz', 'loch ness', 'dziura', 'lina',
+    'miedź', 'kręgi', 'grecja', 'perła', 'lis', 'rama', 'ambulans', 'łożysko', 'promień', 'miód', 'duch', 'wydech', 'drzewo',
+    'but', 'klamka', 'kot', 'pas', 'czar', 'nić', 'pupil', 'choroba', 'śmierć', 'maks'
+
 ]
 
 function getRandom(number){
