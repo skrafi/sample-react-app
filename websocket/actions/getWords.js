8d4436fbd040aad4297c902b2140db1a73893b99
JN <git@istalacar.com>
2020-04-17 22:12:22 +0200

    Add more words times 2

--- websocket/actions/getWords.js
+++ websocket/actions/getWords.js
@@ -11,7 +11,7 @@ const WORDS = [
     'pociąg', 'spadek', 'żabka', 'placek', 'rzęsa',
     'rzut', 'guma', 'soczewka','kontrakt', 'kiwi',
     'klucz', 'masa', 'jagoda', 'rura', 'usta',
-    'ogon', 'dusza', 'królik','superbohater', 'bal',
+    'ogon', 'królik','superbohater', 'bal',
     'księżniczka','trucizna', 'chochlik', 'klawisz', 'mistrz',
     'stołek', 'kciuk', 'szkoła', 'most', 'bomba',
     'ruletka', 'statek', 'trawa', 'siano', 'lew',
@@ -36,8 +36,29 @@ const WORDS = [
     'szczypior', 'splot', 'ameryka', 'pierścień', 'tusz', 'szekspir', 'europa', 'rzym', 'zieleń', 'krówka', 'wieżowiec',
     'podkowa', 'pasta', 'moskwa', 'fartuch', 'sokół', 'maj', 'gwiazda', 'siekacz', 'loch ness', 'dziura', 'lina',
     'miedź', 'kręgi', 'grecja', 'perła', 'lis', 'rama', 'ambulans', 'łożysko', 'promień', 'miód', 'duch', 'wydech', 'drzewo',
-    'but', 'klamka', 'kot', 'pas', 'czar', 'nić', 'pupil', 'choroba', 'śmierć', 'maks'
-
+    'but', 'klamka', 'kot', 'pas', 'czar', 'nić', 'pupil', 'choroba', 'śmierć', 'maks',
+    'płyta', 'wstęp', 'siła', 'góra', 'kucharz', 'laska', 'kret', 'pielęgniarka', 'tusza', 'bąk', 'afryka',
+    'ruda', 'kraków', 'plastik', 'wieża', 'słup', 'rekin', 'donice', 'policja',
+    'koń', 'egipt', 'tokio', 'kaptur', 'oliwa', 'szafa', 'zebra',
+    'pająk', 'keczup', 'pan', 'torebka', 'kaczor', 'zespół', 'ośmiornica', 'samochód', 'diament', 'doktor',
+    'komórka', 'ninja', 'australia', 'koło', 'Nowy York', 'Polska', 'Żelazo', 'piramida', 'strzał',
+    'szczęście', 'gigant', 'Waszyngton', 'wkład', 'orzech', 'lew', 'golf', 'poczta',
+    'łuk', 'stół', 'jatka', 'dywan', 'stopień', 'Dusza', 'pokrywka', 'Dania', 'kiwi', 'gaz', 'kość',
+    'sznur', 'usta', 'czapa', 'nurek', 'himalaje', 'stan', 'mistrz', 'śnieg', 'żubr', 'amazonka', 
+    'bałwan', 'bicz', 'bal', 'rzęsa', 'lot', 'twarz', 'łódź', 'plaża',
+    'prawnik', 'pole', 'guzik', 'mysz', 'Niemcy', 'połączenie', 'wachlarz', 'głowa',
+    'oko', 'satelita', 'gra', 'mikroskop', 'nauczyciel', 'bar', 'niebo', 'nora', 'północ',
+    'holender', 'model', 'kamień', 'flet', 'olej', 'jabłko', 'bomba', 'jednorożec', 'pochodnia', 'królowa',
+    'opoka', 'naukowiec', 'Londyn', 'noga', 'krzyż', 'gracja', 'kolec', 'wieloryb', 'wybuch', 'basen', 'centaur',
+    'gładki', 'szczyt', 'świerszcz', 'talerz', 'foka', 'centrum', 'orzeł',
+    'kontakt', 'zamek', 'cień', 'stopa', 'trójkąt', 'bawełna', 'ucho', 'szkło', 'sztuka', 'klatka', 'linia',
+    'robot', 'złoto', 'waga', 'fala', 'szkocja', 'ręka', 'świnia', 'rękawica', 'ziemia', 'rakieta',
+    'materiał', 'tuba', 'laser', 'trąba', 'kwadrat', 'opera', 'kangur', 'serce',
+    'anglia', 'lakier', 'beczka', 'hotel', 'rząd', 'pistolet', 'język', 'figura', 'humor',
+    'kropka', 'widelec', 'tchórz', 'nóż', 'babka', 'złodziej', 'wiosna', 'szmugiel', 'zmiana', 'krzesło', 'bermudy',
+    'napad', 'paleta', 'meksyk', 'kozioł', 'kościół', 'antarktyda', 'płot', 'dwór', 'konar', 'kostium',
+    'pilot', 'groszek', 'żołnierz', 'kalosz', 'korzenie', 'taniec', 'mamut', 'róża', 'smok',
+    'toaleta', 'robak', 'karta', 'czujka', 'koncert', 'Hollywood', 'kasa', 'nos'
 ]
 
 function getRandom(number){
